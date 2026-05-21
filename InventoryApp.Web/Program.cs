0fd56208ea4de87314af10e3dec9550f176d0559
nomad <boburfayzullayev19avgust@gmail.com>
2026-05-21 14:48:52 +0500

    Fix all projects to net10.0

--- InventoryApp.Web/Program.cs
+++ InventoryApp.Web/Program.cs
@@ -17,8 +17,7 @@ builder.Services.AddAntiforgery(options =>
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
 {
     options.Password.RequireDigit = true;
